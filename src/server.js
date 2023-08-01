b331da42587cb1c7593fdd0f57abe83ff751be79
PierreJzql <pierrejezequel92@gmail.com>
2023-08-01 22:39:27 +0200

    create views

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from 'cors';
 import routes from "./routes/webhook.js"
@@ -9,16 +8,12 @@ dotenv.config();
 const app = express();
 const port = parseInt(process.env.PORT) || 3000;
 
-app.use(morgan("dev"));
+app.use(express.static("./src/views/"));
+
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use(express.static("./src/public"));
-
 app.use("/", routes);
-
-
 
 
 app.listen(port, () => {
---
+++ src/views/js/chatbox.js
@@ -0,0 +1,3 @@
+let chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "112664131915704");
+chatbox.setAttribute("attribution", "biz_inbox");
