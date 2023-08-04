f47ac1940ff8ad75abd1c3504ba278a47dcad8c0
PierreJzql <pierrejezequel92@gmail.com>
2023-08-04 17:33:38 +0200

    bug fix

--- src/views/js/chatbox.js
+++ src/views/js/chatbox.js
@@ -1,8 +1,4 @@
-import dotenv from "dotenv";
-
-dotenv.config()
-
-const idPlugIn = process.env.ID_PLUGIN
+const idPlugIn = process.env.ID_PLUGIN;
 
 let chatbox = document.getElementById('fb-customer-chat');
 chatbox.setAttribute("page_id", idPlugIn);
--- test.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config()
-
-const idPlugIn = process.env.ID_PLUGIN
-
-console.log(idPlugIn)
